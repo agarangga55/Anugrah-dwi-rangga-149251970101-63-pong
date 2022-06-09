1e50cf71f232e56490832af1589948e1ffb89e49
Koga <79469238+agarangga55@users.noreply.github.com>
2022-06-09 13:43:05 +0700

    Adding UI & Game Manager
    menambahkan UI dan game manager pada permainan pong

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Created By Anugrah Dwi Rangga - 149251970101-63");
+        SceneManager.LoadScene("Gameplay");
+        Debug.Log("Created By Anugrah Dwi Rangga - 149251970101-63");
+    }
+
+    public void OpenExtras()
+    {
+        Debug.Log("Ini Tombol");
+        Debug.Log("h3h3h3h3");
+        Debug.Log("Created By Anugrah Dwi Rangga - 149251970101-63");
+    }
+}
